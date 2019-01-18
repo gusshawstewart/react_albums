529ec76e9d1193f1854f537530ad272e22ffc3fd
gusshawstewart <g.shawstewart@gmail.com>
2019-01-18 17:12:14 +0000

    lecture 25 secion 6

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -1,35 +1,25 @@
-// Import libraries for making a component
+// import libraries for making a component
 import React from 'react';
 import { Text, View } from 'react-native';
 
-// Make a component
-const Header = (props) => {
+//then make a component
+const Header = () => {
   const { textStyle, viewStyle } = styles;
-
   return (
     <View style={viewStyle}>
-      <Text style={textStyle}>{props.headerText}</Text>
+      <Text style={textStyle}>Albums!</Text>
     </View>
-  );
+  )
 };
 
 const styles = {
   viewStyle: {
-    backgroundColor: '#F8F8F8',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 60,
-    paddingTop: 15,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    elevation: 2,
-    position: 'relative'
+    backgroundColor: '#F8F8F8'
   },
   textStyle: {
-    fontSize: 20
+    fontSize: 40
   }
 };
 
-// Make the component available to other parts of the app
+//then make the component available to other parts of the app
 export default Header;
